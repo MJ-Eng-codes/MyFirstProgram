81f8296cfb1646942d869494ea6bffdf338bd75e
swany <swany@MSI>
2024-09-09 15:26:10 +0200

    Add project files.

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MyFirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNumber = 40;
+            if (myNumber > 40)
+            {
+                Console.WriteLine("myNumber är större än 40");
+            }
+            
+        }
+    }
+}
